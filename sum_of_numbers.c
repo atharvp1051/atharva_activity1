0fbd478e4f80e86440879c5af44099c34aede59e
eng19cs0052-atharva <76119710+eng19cs0052-atharva@users.noreply.github.com>
2021-02-04 13:45:36 +0530

    Before Compiling

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,13 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include<stdio.h>
+int main()
+{   
+    int a,b,s;
+    printf("Enter First Number: ");
+    scanf("%d",a);
+    printf("Enter Second Number: ");
+    scanf("%d",b);
+    s=a+b;
+    printf("Sum is: %d",s);
+    return(0);
+}
