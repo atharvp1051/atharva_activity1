488375d602c7bdb66031f4f18c2a9a861a0ff692
eng19cs0052-atharva <76119710+eng19cs0052-atharva@users.noreply.github.com>
2021-02-25 12:49:00 +0530

    update v2

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,10 +1,11 @@
 //Write a program to find vol of tromboloid
 #include<stdio.h>
-void main()
+int main()
 {
-float h,d,b,v;
-printf("Enter the Dimensions of tromboloid in format Height Depth Breadth");
-scanf("%f,%f,%f",&h,&d,&b);
-v=(0.333)*((h*d*b)+(d/b));
-printf("Volume is %f.",v);
+    float h,d,b,v;
+    printf("Enter the Dimensions of tromboloid in format Height Depth Breadth");
+    scanf("%f %f %f",&h,&d,&b);
+    v=(0.333)*((h*d*b)+(d/b));
+    printf("Volume is %f.",v);
+    return 0;
 }
